5c371cc5b832ff170cbe67166d1d35c7378945ab
cob <cobj24@student.bth.se>
2025-04-23 14:58:30 +0200

    kmom02 first commit

--- src/Controller/LuckyControllerJson.php
+++ src/Controller/LuckyControllerJson.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Card\DeckOfCards;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -44,6 +47,51 @@ class LuckyControllerJson extends AbstractController
             'quote' => $randomQuote,
             'date' => $now->format('Y-m-d'),
             'timestamp' => $now->format('H:i:s'),
+        ]);
+    }
+
+    #[Route('/api/deck', name: 'api_deck', methods: ['GET'])]
+    public function deck(SessionInterface $session): JsonResponse
+    {
+        $deck = $session->get('deck', new DeckOfCards(true));
+        return $this->json($deck->getCards());
+    }
+
+    #[Route('/api/deck/shuffle', name: 'api_shuffle', methods: ['POST'])]
+    public function shuffle(SessionInterface $session): JsonResponse
+    {
+        $deck = new DeckOfCards(true);
+        $deck->shuffle();
+        $session->set('deck', $deck);
+
+        return $this->json($deck->getCards());
+    }
+
+    #[Route('/api/deck/draw', name: 'api_draw_one', methods: ['POST'])]
+    public function drawOne(SessionInterface $session): JsonResponse
+    {
+        /** @var DeckOfCards $deck */
+        $deck = $session->get('deck', new DeckOfCards(true));
+        $drawn = $deck->draw();
+        $session->set('deck', $deck);
+
+        return $this->json([
+            'drawn' => $drawn,
+            'remaining' => $deck->count()
+        ]);
+    }
+
+    #[Route('/api/deck/draw/{number<\d+>}', name: 'api_draw_number', methods: ['POST'])]
+    public function drawNumber(SessionInterface $session, int $number): JsonResponse
+    {
+        /** @var DeckOfCards $deck */
+        $deck = $session->get('deck', new DeckOfCards(true));
+        $drawn = $deck->draw($number);
+        $session->set('deck', $deck);
+
+        return $this->json([
+            'drawn' => $drawn,
+            'remaining' => $deck->count()
         ]);
     }
 }
