f1d6cb35b3cb311d72669407bcf370c6b5205405
cob <cobj24@student.bth.se>
2025-05-21 11:24:18 +0200

    kmom05 changed image path

--- src/DataFixtures/BookFixtures.php
+++ src/DataFixtures/BookFixtures.php
@@ -11,9 +11,9 @@ class BookFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $books = [
-            ['title' => 'Kafka på stranden', 'isbn' => '9789113132969', 'author' => 'Haruki Murakami', 'image' => '/img/kafka.jpg'],
-            ['title' => 'Illusionisten', 'isbn' => '9789178935123', 'author' => 'John Fowles', 'image' => '/img/illusionisten.jpg'],
-            ['title' => 'M Train', 'isbn' => '9781408867709', 'author' => 'Patti Smith', 'image' => '/img/mtrain.jpg'],
+            ['title' => 'Kafka på stranden', 'isbn' => '9789113132969', 'author' => 'Haruki Murakami', 'image' => 'img/kafka.jpg'],
+            ['title' => 'Illusionisten', 'isbn' => '9789178935123', 'author' => 'John Fowles', 'image' => 'img/illusionisten.jpg'],
+            ['title' => 'M Train', 'isbn' => '9781408867709', 'author' => 'Patti Smith', 'image' => 'img/mtrain.jpg'],
         ];
 
         foreach ($books as $b) {
