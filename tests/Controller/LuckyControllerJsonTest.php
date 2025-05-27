436ef80c69c01ac55ff4488245fe38792236947a
cob <cobj24@student.bth.se>
2025-05-27 09:24:37 +0200

    kmom06 uppdaterade filer

--- tests/Controller/LuckyControllerJsonTest.php
+++ tests/Controller/LuckyControllerJsonTest.php
@@ -4,7 +4,7 @@ namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class LuckyControllerTest extends WebTestCase
+class LuckyControllerJsonTest extends WebTestCase
 {
     public function testLuckyRouteReturnsValidJson(): void
     {
